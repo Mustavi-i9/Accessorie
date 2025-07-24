8d8fa7bbb08019968e14f91a244846a49a4699c6
Mustavi-i9 <mustavialmueid@gmail.com>
2025-07-25 02:50:09 +0600

    update

---
+++ web accessories /Programming Accessories.c
@@ -0,0 +1,98 @@
+	if (condition = true) {		// i
+        
+    } else if (condition) {
+        
+    } else {
+        
+    } 
+    
+    
+    if (condition == true) {	// ii
+        
+    } else {
+        
+    } 
+    
+   
+    else if (condition) {		 // u
+        
+    } 
+    
+    if (condition = true) {		// if
+        
+    } 
+    
+    
+    else if (condition) {		// ei
+        
+    } 
+    
+    
+    
+    for (int i = 0; i <= length; i++) {   // f
+        
+    } 
+    
+    while (condition) { // w
+        
+    } 
+    
+    do while 
+    
+	
+	switch (condition) {
+		case 1:
+			
+			break;
+		case 2:
+			// Tab to edit
+			break;
+		
+		default:
+			break;
+    }
+    
+    
+    
+    
+    
+    char *name = ;
+	int variable = value; 
+    
+    int variable;			// nt
+    float variable;			// fl
+    char variable;			// ch
+    double variable;		// dl
+    long double variable;	// lg
+    
+	void name(param) {		// fn
+        
+    } 
+    
+    printf("something \n"); 			  // pp		
+    printf("something");				  // p
+    scanf("%format_specifier",&Variable); // o
+    printf("%format_specifier",variable); // L
+    
+   
+    
+#include <stdio.h>
+
+int main() {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    printf("\n\n\n\n\n\n\n\n\n\n"); 
+    return 0;
+} 
